2a586cc108a4d99dd070282af3793cb16698bd25
aditybikramswain <adityabikram1999@gmail.com>
2025-10-28 10:38:46 +0530

    Add folder

---
+++ SwiftBootCamp/Offset _Based _Pagination/Model/NatureModel.swift
@@ -0,0 +1,8 @@
+//
+//  NatureModel.swift
+//  SwiftBootCamp
+//
+//  Created by Apple on 28/10/25.
+//
+
+import Foundation
---
+++ SwiftBootCamp/Offset _Based _Pagination/ViewModel/NatureViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  NatureViewModel.swift
+//  SwiftBootCamp
+//
+//  Created by Apple on 28/10/25.
+//
+
+import Foundation
+import Combine
+
+class NatureViewModel: ObservableObject {
+    
+}
