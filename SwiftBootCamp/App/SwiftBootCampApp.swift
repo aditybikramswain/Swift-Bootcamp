91e3aa9ec21d3e807a07072e770f054ad0356170
aditybikramswain <adityabikram1999@gmail.com>
2025-10-28 10:25:01 +0530

    Add initial SwiftBootCamp files

---
+++ SwiftBootCamp/App/SwiftBootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftBootCampApp.swift
+//  SwiftBootCamp
+//
+//  Created by Apple on 28/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftBootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
